48a5cd49d5a46b90c1538d3f18931f3a41d02b25
pchel <pchel@zuumapp.com>
2023-06-26 02:45:39 -0300

    fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                nested: resolve(__dirname, 'nested/index.html'),
+            },
+        },
+    },
 })
